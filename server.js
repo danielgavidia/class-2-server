5f6d69cec1c3c9e2e52eb9605b4e1812940a5662
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-02 19:22:15 -0400

    added get method

--- server.js
+++ server.js
@@ -6,9 +6,26 @@ const port = 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Use json
+app.use(express.json());
+
 // Set up a route to serve the index.html file
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// -----
+const data = [
+    { id: 1, name: "Alice", email: "alice@example.com" },
+    { id: 2, name: "Bob", email: "bob@example.com" },
+    { id: 3, name: "Charlie", email: "charlie@example.com" },
+];
+
+// Get
+app.get("/id:", (req, res) => {
+    const id = req.params.id;
+    const user = data.find((u) => u.id === id);
+    res.json(user);
 });
 
 app.listen(port, () => {
